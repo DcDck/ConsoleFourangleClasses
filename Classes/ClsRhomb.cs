cbd7861b6f48830ff481d37a81fccf8f2405f2c2
admin <admin@44-10>
2023-04-24 14:01:05 +0300

    Добавьте файлы проекта.

---
+++ Classes/ClsRhomb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFourangleClasses.Classes
+{
+    internal class ClsRhomb : ClsParallelogram
+
+    {
+        private double sideA;
+
+        public void SidesCalculation()
+        {
+            sideA = Math.Sqrt((X1 - X2) * (X1 - X2) + (Y1 - Y2) * (Y1 - Y2));
+            Console.WriteLine("Сторона ромба");
+            Console.WriteLine($"Стороная A={Math.Round(sideA, 2)}");
+        }
+        public double Perimetr()
+        {
+            return 4 * sideA;
+        }
+        public double Ploshad()
+        {
+            double s = 0;
+            double p = Perimetr() / 2.0;
+            s = Math.Pow(sideA,2) * Math.Sqrt(X1);
+            return s;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using ConsoleFourangleClasses.Classes;
+
+Console.WriteLine("Чектырехугольник1");
+ClsFourangle fourngle1 = new ClsFourangle();
+fourngle1.KeyboardInput();
+fourngle1.SidesCalculation();
+fourngle1.DiagonalsCalculation();
+Console.WriteLine($"Периметр = {fourngle1.Perimetr()}");
+Console.WriteLine($"Площадь = {fourngle1.Ploshad()}");
+
+Console.WriteLine("Четырехугольник 2");
+ClsFourangle fourngle2 = new ClsFourangle(-5, 4, 8, 6, 9, -4, -4, -3);
+
+fourngle2.SidesCalculation();
+fourngle2.DiagonalsCalculation();
+Console.WriteLine($"Периметр = {fourngle2.Perimetr()}");
+Console.WriteLine($"Площадь = {fourngle2.Ploshad()}");
+
+ClsParallelogram parall = new ClsParallelogram();
+parall.KeyboardInput();
+parall.SidesCalculation();
+parall.DiagonalsCalculation();
+parall.HeightCalculation();
+Console.WriteLine($"Периметр = {parall.Perimetr()}");
+Console.WriteLine($"Площадь = {parall.Ploshad()}");
+
+ClsRectangle rec = new ClsRectangle();
+rec.KeyboardInput();
+rec.SidesCalculation();
+rec.DiagonalsCalculation();
+Console.WriteLine($"Периметр = {rec.Perimetr()}");
+Console.WriteLine($"Площадь = {rec.Ploshad()}");
+
+ClsRhomb rhomb = new ClsRhomb();
+rhomb.KeyboardInput();
+rhomb.SidesCalculation();
+rhomb.DiagonalsCalculation();
+Console.WriteLine($"Периметр = {rhomb.Perimetr()}");
+Console.WriteLine($"Площадь = {rhomb.Ploshad()}");
